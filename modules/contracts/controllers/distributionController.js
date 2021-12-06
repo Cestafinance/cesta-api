fa3e1317e09adeb65cc59fc7a1b0790628c841ec
Phyllice Wai <waikahkwan0903@gmail.com>
2021-12-06 10:59:01 +0800

    refactor: Move asset distribution function to util file.

--- modules/contracts/controllers/distributionController.js
+++ modules/contracts/controllers/distributionController.js
@@ -1,6 +1,4 @@
-const { ASSET_DISTRIBUTION } = require("../constants/distribution/index");
-const { COINGECKO_URL } = require("../constants/distribution/coingekcoIds");
-const { TOKEN_CHART_COLOR, BACKUP_CHART_COLOR } = require("../constants/distribution/chartColors");
+const { getAssetDistribution } = require("../utils/distribution-util");
 
 module.exports = () => {
 
@@ -8,7 +6,7 @@ module.exports = () => {
     const imports = module.getImports();
     const options = module.getOptions();
 
-    const getAssetDistribution = async (strategyId, minInfo = true) => {
+    const distributionHandler = async (strategyId, minInfo = true) => {
         // Min Info : Returns only coingecko url, percentage, color, label, link, percent, token id
         
         let result = [];
@@ -16,47 +14,19 @@ module.exports = () => {
             if(strategyId === undefined || strategyId === "") {
                 throw(`Missing strategy id`);
             }
-            strategyId = strategyId.toUpperCase();
-
-            const tokens = ASSET_DISTRIBUTION[strategyId];
-            if(tokens === undefined) {
-                throw(`Assets for ${strategyId} is undefined.`);
-            } 
-
-            let index = 0;
-            result = Object.keys(tokens).map(key => {
-                // Build coingecko url
-                const token = tokens[key];
-
-                const coingeckoId = token.tokenId;
-                const coingeckoURL = `${COINGECKO_URL}${coingeckoId}`;
-
-                // Get chart color
-                let chartColor = TOKEN_CHART_COLOR[coingeckoId];
-                if(chartColor === undefined) {
-                    chartColor = BACKUP_CHART_COLOR[index];
-                    index ++;
-                }
-
-                return {
-                    ...{...token}, 
-                    label: key,
-                    link: coingeckoURL,
-                    color: chartColor
-                }
-            });
-            
+           
+            result = await getAssetDistribution(strategyId);
 
             return result;
 
         } catch (err) {
-            console.error(`Error in getAssetDistribution(): `, err);
+            console.error(`Error in distributionHandler(): `, err);
         } 
     }
 
 
     return {
-        getAssetDistribution
+        distributionHandler
     }
 
 }
---
+++ modules/contracts/utils/distribution-util.js
@@ -0,0 +1,41 @@
+const {ASSET_DISTRIBUTION} = require("../constants/distribution/index");
+const {TOKEN_CHART_COLOR, BACKUP_CHART_COLOR } = require("../constants/distribution/chartColors");
+const { COINGECKO_URL } = require("../constants/distribution/coingekcoIds");
+
+const getAssetDistribution = async(strategyId) => {
+    strategyId = strategyId.toUpperCase();
+    const tokens = ASSET_DISTRIBUTION[strategyId];
+    if(tokens === undefined) {
+        throw(`Assets for ${strategyId} is undefined.`);
+    } 
+
+    let index = 0;
+    result = Object.keys(tokens).map(key => {
+        // Build coingecko url
+        const token = tokens[key];
+
+        const coingeckoId = token.tokenId;
+        const coingeckoURL = `${COINGECKO_URL}${coingeckoId}`;
+
+        // Get chart color
+        let chartColor = TOKEN_CHART_COLOR[coingeckoId];
+        if(chartColor === undefined) {
+            chartColor = BACKUP_CHART_COLOR[index];
+            index ++;
+        }
+
+        return {
+            ...{...token}, 
+            label: key,
+            link: coingeckoURL,
+            color: chartColor
+        }
+    });
+
+    return result;
+    
+}
+
+module.exports = {
+    getAssetDistribution
+}
